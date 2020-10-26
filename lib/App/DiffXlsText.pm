0c441e7c54f5fa3567d129653a48919903d274a9
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-10-26 20:24:38 +0700

    Initial commit

---
+++ lib/App/DiffXlsText.pm
@@ -0,0 +1,15 @@
+package App::DiffXlsText;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Diff the text of two Office spreadsheets (.ods, .xls, .xlsx) as two directories of CSV files
+
+=head1 SYNOPSIS
+
+See the included script L<diff-xls-text>.
+
+=cut
